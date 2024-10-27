fbb540183ca67ca364581f8b6a7eaf1589ad4a47
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2024-10-27 15:35:10 +0100

    Delete day00/ex11/lib directory

--- day00/ex11/lib/prediction copy.py
+++
@@ -1,9 +0,0 @@
-def simple_predict(x_values, theta):
-    y = []
-    for x in x_values:
-        y.append(theta[0] + (theta[1] * x))
-    return y
-
-# x_values = [1, 2, 3 ,4, 5]
-# theta = [5, 3]
-# print(simple_predict(x_values, theta))
